1d15eb3d9313b33f1af7f7c5c8e4815674aad025
anton <sirmaahw@gmail.com>
2017-09-21 01:24:20 +0300

    update

--- web/router.py
+++ web/router.py
@@ -6,5 +6,5 @@ from web.handlers import index
 Route = namedtuple('Route', ['name', 'method', 'path', 'handler'])
 
 routes = [
-    Route('index', 'GET', '/', index),
+    Route('index', 'POST', '/', index),
 ]
