daee8154e0f45ecd938b7f159f13264dcdca3336
anton <sirmaahw@gmail.com>
2017-09-29 01:09:55 +0300

    postgres

--- web/settings.py
+++ web/settings.py
@@ -4,7 +4,21 @@ TELEGRAM_SECRET_TOKEN = "qwerty12345"
 
 db_url = os.environ.get('DATABASE_URL')
 
-DB_SETTINGS = {
-    'provider': 'postgres' if db_url else 'sqlite',
-    'filename': db_url or'../database.sqlite'
-}
+if db_url:
+    from urllib.parse import urlparse
+
+    result = urlparse(db_url)
+    username = result.username
+
+    DB_SETTINGS = {
+        'provider': 'postgres',
+        'username': result.username,
+        'password': result.password,
+        'database': result.path[1:],
+        'hostname': result.hostname,
+    }
+else:
+    DB_SETTINGS = {
+        'provider': 'sqlite',
+        'filename': '../database.sqlite'
+    }
