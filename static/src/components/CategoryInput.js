8fe6a9500eeb404ea6a2a8937f631da5caeea5b2
anton <sirmaahw@gmail.com>
2017-10-13 14:18:50 +0300

    fix

--- static/src/components/CategoryInput.js
+++ static/src/components/CategoryInput.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
 export default class CategoryInput extends Component {
-  render() {
-    return (
-      <div>
-        <input onChange={ (e) => { this.setState({ category: e.target.value }) } }>{ this.state.category }</input>
-        <button onClick={ this.props.updateCategory(this.state.category) }>сохранить</button>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            category: this.props.category
+        }
+    }
+
+    render() {
+        return (
+          <div>
+            <input onChange={ (e) => { this.setState({ category: e.target.value }) } }>{ this.state.category }</input>
+            <button onClick={ this.props.updateCategory(this.state.category) }>сохранить</button>
+          </div>
+        );
+    }
 }
--- static/src/components/Note.js
+++ static/src/components/Note.js
@@ -3,17 +3,17 @@ import CategoryInput from './CategoryInput'
 
 export default class Note extends Component {
     render() {
-    return (
-      <div>
-        <p>
-            { this.props.note.text }
-            <span onClick={ () => { this.props.delete( this.props.note ) } }>удалить</span>
-            <CategoryInput
-                category={ this.props.category }
-                updateCategory={ (name) => this.props.updateCategory( this.props.note, name )}
-            />
-        </p>
-      </div>
-    );
+        return (
+          <div>
+            <p>
+                { this.props.note.text }
+                <span onClick={ () => { this.props.delete( this.props.note ) } }>удалить</span>
+                <CategoryInput
+                    category={ this.props.category }
+                    updateCategory={ (name) => this.props.updateCategory( this.props.note, name )}
+                />
+            </p>
+          </div>
+        );
     }
 }
