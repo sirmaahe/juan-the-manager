2ac39f14f1429173602467a3bb8f6ba2ef672ebc
anton <sirmaahw@gmail.com>
2017-10-13 12:38:44 +0300

    fix

--- web/utils.py
+++ web/utils.py
@@ -9,6 +9,6 @@ async def send_tg_message(chat_id, message):
     async with aiohttp.ClientSession() as session:
         async with session.post(
             f'https://api.telegram.org/bot{TELEGRAM_BOT_KEY}/sendMessage',
-            data=json.dumps({"chat_id": chat_id, "text": message})
+            json={"chat_id": chat_id, "text": message}
         ) as resp:
             return await resp.json()
